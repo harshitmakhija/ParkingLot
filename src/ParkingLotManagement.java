33ca29a9eceaadcb2ad6fb2373c340f76882ddb6
Harshit <harshit.makhija@rapido.bike>
2022-07-29 14:16:49 +0530

    29 july 2:15pm

--- src/ParkingLotManagement.java
+++ src/ParkingLotManagement.java
@@ -2,65 +2,54 @@ import java.util.Scanner;
 
 public class ParkingLotManagement {
 
-     static int spaceAvailable = 3 ;
+    public int spaceAvailable = 1;
 
-     static Driver getDriverDetailsFromUser()
-     {
-         String driverName, driverContact, driverCarNumber ;
+    static Driver getDriverDetailsFromUser() {
+        String driverName, driverContact, driverCarNumber;
 
-         Scanner scanner = new Scanner(System.in).useDelimiter("\n") ;
-         System.out.println("Enter Driver's Name : ");
-         driverName = scanner.next() ;
-         System.out.println("Enter Driver's Contact : ");
-         driverContact = scanner.next();
-         System.out.println("Enter Driver's Car Number : ");
-         driverCarNumber = scanner.next();
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("Enter Driver's Name : ");
+        driverName = scanner.next();
+        System.out.println("Enter Driver's Contact : ");
+        driverContact = scanner.next();
+        System.out.println("Enter Driver's Car Number : ");
+        driverCarNumber = scanner.next();
 
-         return new Driver(driverName, driverContact, driverCarNumber);
-     }
+        return new Driver(driverName, driverContact, driverCarNumber);
+    }
 
-    static ParkingTicket createParkingTicket()
-    {
-        int parkingID = spaceAvailable ;
-        spaceAvailable-=1;
+    public ParkingTicket createParkingTicket() {
+        int parkingID = spaceAvailable;
 
-        String checkInDate = "Random Date" ;
-        String checkInTime = "Random Time" ;
+        String checkInDate = "Random Date";
+        String checkInTime = "Random Time";
 
         return new ParkingTicket(parkingID, checkInDate, checkInTime);
     }
 
-     static String parkMyCar()
-     {
-         Driver driverObject = getDriverDetailsFromUser() ;
-         driverObject.parkingTicket = createParkingTicket();
-
-         return "CAR PARKED" ;
-     }
-
-     static boolean checkIfParkingSlotAvailable()
-     {
-         return spaceAvailable > 0 ;
-     }
+    public String parkMyCar() {
+        spaceAvailable--;
+        return "CAR PARKED";
+    }
 
 
-    public static void main(String [] args)
-    {
-        System.out.println("Welcome to the Parking Management System!");
+    public boolean checkIfParkingSlotAvailable() {
+        return spaceAvailable > 0 ? true : false;
+    }
 
+    public String GetStatus() {
         if (checkIfParkingSlotAvailable()) {
-            System.out.println("Space is Available, you can park") ;
-
-            parkMyCar();
+            return parkMyCar();
+        } else {
+            return "NO SLOT AVAILABLE";
         }
-        else{
-            System.out.println("No Space Available!! Sorry");
-
-        }
+    }
 
 
-
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Parking Management System!");
+        ParkingLotManagement parkingLotManagement = new ParkingLotManagement();
+        parkingLotManagement.GetStatus();
 
 
     }
